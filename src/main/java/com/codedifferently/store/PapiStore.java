13cc40142dd5f29764c329efec34bbba92ed4d73
Abiodun Giwa <biodun1230@gmail.com>
2020-11-23 14:47:28 -0500

    papi store comments completed.

--- src/main/java/com/codedifferently/store/PapiStore.java
+++ src/main/java/com/codedifferently/store/PapiStore.java
@@ -1,20 +1,22 @@
+// This is the package it defines the family relation of objects
 package com.codedifferently.store;
-
+// this imports ArrayList, HashMap & Map classes from java.util library/folder
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+// this is PapiStore class that contains the scope of this class
 public class PapiStore {
+    // declaring variables tabs, isStoreOpen & owesPapiMoney
     private boolean isStoreOpen;
     private ArrayList<String> tabs;
     Map<String, Double> owesPapiMoney;
-
+    // this is a constructor that initializes the declared variables in the PapiStore class
     public PapiStore(){
         this.isStoreOpen = false;
         this.tabs = new ArrayList();
         this.owesPapiMoney = new HashMap<>();
     }
-
+    // a constructor that takes a boolean and ArrayList parameters
     public PapiStore(Boolean isOpen, ArrayList<String> tabsIn){
         this.isStoreOpen = isOpen;
         this.tabs = tabsIn;
@@ -24,23 +26,23 @@ public class PapiStore {
     public void closeStore(){
 
     }
-
+    // this is a method called openStore that returns void and is used to check if the store is open
     public void openStore(){
         this.isStoreOpen = true;
     }
-
+    // this is a method that returns the true/false state depending on if the store is open
     public boolean isStoreOpen() {
         return isStoreOpen;
     }
 
-
+    // a boolean method that will return true if the name in the parameter has an open tab at the papi store
     public Boolean allowToUseTab(String name){
         Boolean response = false;
         if(hasAnActiveTab(name) && !hasAnOverDueBalance(name))
             response = true;
         return response;
     }
-
+    // a private method that returns true/false depending on if the name in its parameter has an overdue balance at papi store
     private boolean hasAnOverDueBalance(String name){
         if(!owesPapiMoney.containsKey(name))
             return false;
@@ -50,7 +52,7 @@ public class PapiStore {
             owes = true;
         return owes;
     }
-
+    // this is a private method that returns true/false if the customer has an active tab or not
     private boolean hasAnActiveTab(String name){
         for (String tab : tabs){
             if (tab.equals(name))
@@ -58,12 +60,12 @@ public class PapiStore {
         }
         return false;
     }
-
+    // this method does not return anything but will create/start a tabe for the name passed in or added
     public void createActiveTab(String name){
         this.tabs.add(name);
     }
 
-
+    // this is the psvm that sends out a greeting when we access papi store.
     public static void main(String[] args) {
         System.out.println("Welcome to Papi Store, Hola");
     }
--- src/test/java/com/codedifferently/store/PapiStoreTest.java
+++ src/test/java/com/codedifferently/store/PapiStoreTest.java
@@ -1,17 +1,18 @@
 // This is the package it defines the family relation of objects
 package com.codedifferently.store;
 
-// This is importing the Assert Class from the org.junit package or folder
+// This is importing the Assert, Before & Test Classes from the org.junit package/folder
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-// This is a public class called Papi
+// This is a public class called PapiStoreTest which will hold the scope of this class
     public class PapiStoreTest {
-
+        // declaring a reference papiStore of type PapiStore
         PapiStore papiStore;
-
+        // this is a fixture called Before which is used when we want to execute some codes before running the test.
         @Before
+        // method called setup that returns nothing/void.  initializes a variation of the reference papiStore.
         public void setup(){
             papiStore = new PapiStore();
             papiStore.createActiveTab("Jamal");
@@ -19,7 +20,7 @@ import org.junit.Test;
             papiStore.owesPapiMoney.put("Jamal", 0.0);
             papiStore.owesPapiMoney.put("Victor", 7.0);
         }
-
+        // test if store is open
         @Test
         public void OpenStoreTest(){
             //Given
@@ -34,7 +35,7 @@ import org.junit.Test;
             //Assert.assertEquals(expected, actual);
             Assert.assertTrue(actual);
         }
-
+        // success test for allowToUseTab
         @Test
         public void allowToUseTabTestSuccess(){
             // given
@@ -47,7 +48,7 @@ import org.junit.Test;
             // then
             Assert.assertEquals(expected, actual);
         }
-
+        // failure test for allowToUseTab
         @Test
         public void allowToUseTabTestFail(){
             // given
